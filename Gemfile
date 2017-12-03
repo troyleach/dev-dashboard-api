3b58429db22e4f165a6a6aead6918538a19df23a
Troy Leach <troyleach29@gmail.com>
2017-12-02 22:41:07 -0600

    added pagination and serialziers

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ end
 gem 'bcrypt', '~> 3.1.11'
 gem 'jwt'
 gem 'active_model_serializers'
+gem 'will_paginate', '~> 3.1.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
--- app/serializers/todo_serializer.rb
+++ app/serializers/todo_serializer.rb
@@ -1,10 +1,8 @@
-module V1
-  class TodoSerializer < ActiveModel::Serializer
-    attributes :id,
-      :title,
-      :created_by,
-      :created_at,
-      :updated_at
-    has_many :items
-  end
+class TodoSerializer < ActiveModel::Serializer
+  attributes :id,
+    :title,
+    :created_by,
+    :created_at,
+    :updated_at
+  has_many :items
 end
